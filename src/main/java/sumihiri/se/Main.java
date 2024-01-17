09054fbbaa6ec5f9cfa6b905fbf39c0529e44b11
Sumihiri <148192770+Sumihiri@users.noreply.github.com>
2024-01-17 22:07:51 +0100

    add new yml CI file

--- src/main/java/sumihiri/se/Main.java
+++ src/main/java/sumihiri/se/Main.java
@@ -2,6 +2,6 @@ package sumihiri.se;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello my name is Jekyll!");
     }
 }
